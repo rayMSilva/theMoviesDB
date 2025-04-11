037d30491a5df5dbd54e24ed6e3bd0cbbccb39cc
rayMSilva <raydasilvaknapp@hotmail.com>
2025-04-11 17:59:26 -0300

    testando novos widgets, scaffold, appBar, Drawer, ElevatedButtons, Containers

--- mestrafacil/lib/main.dart
+++ mestrafacil/lib/main.dart
@@ -1,19 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp( // Root widget
       home: Scaffold(
-        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: const Text('My Home Page'),
+        ),
         body: Center(
-          child: Text('Primeira aplicação flutter'),
+          child: Builder(
+            builder: (context) {
+              return Column(
+                children: [
+                  Container(
+                    color: Colors.red,
+                    padding: EdgeInsets.all(20.0),
+                    margin: EdgeInsets.all(10.0),
+                    child: Text('Este é o primeiro botao vvvvv')  
+                  ),
+                  Container(
+                    child: ElevatedButton(onPressed: ()
+                    {
+                      print('Olá eu sou o 1° botao');
+                    },child: const Text('1° butao')),
+                  ),
+                  const Text('Este é o segundo botao vvvv'),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      http
+                      .get(
+                        Uri.https('xptoflutter.free.beeceptor.com', '/test')
+                        )
+                      .then(
+                        (value) => print('Olá eu sou o 2° botao API.Body => ${value.body}')
+                        );
+                    },
+                    child: const Text('2° butao'),
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
